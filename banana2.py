4df80030394496cdcd0ca3e138567aa36d27f13a
star <star_su@brown.edu>
2019-01-26 19:23:32 -0500

    oz!

--- banana2.py
+++ banana2.py
@@ -1,7 +1,9 @@
 from cmd import Cmd
+from gensim.models import Word2Vec
 from collections import Counter
 import nltk
 from nltk.tokenize import sent_tokenize 
+from nltk.corpus import stopwords
 import gzip
 import logging
 import re
@@ -26,6 +28,13 @@ class Prompt(Cmd):
 				'black', 'white', 'grey'] 
 
 	def do_find_neighbors(self, args):
+		"""give me [color] [text]"""
+		command = args.split()
+		color = command[0]
+		file = command[1]
+		color_corpus = open(command[1], 'w+')
+
+		corpus = open(command[1], encoding='utf8').read().lower()
 		stoplist = set(stopwords.words('english'))
 		clean = [word for word in corpus.split() if word not in stoplist]
 		clean = ' '.join(clean)
@@ -34,14 +43,22 @@ class Prompt(Cmd):
 		text = list(map(lambda x: x.split(), text))
 		#clean_text = [x for x in text if x]
 
-		sentences = clean_text
+		sentences = text
 		model = Word2Vec(sentences, min_count=1)
 		words = list(model.wv.vocab)
 		model.save('model.bin')
 		# load model
 		new_model = Word2Vec.load('model.bin')
-		color1 = ['red']
+		color1 = [color]
 		print(model.wv.most_similar(positive=color1,topn=10))
+
+		for color in self.color_list:
+			sys.stdout.write(color + " ")
+			neighbors = model.wv.most_similar(positive=color1,topn=10)
+			for neighbor, val in neighbors:
+				sys.stdout.write(neighbor + " ")
+			sys.stdout.write("\n")
+		sys.stdout.flush()
 		pass
 
 	def do_find_colors(self, args):
